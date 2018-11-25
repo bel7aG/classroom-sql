6be33aefe15e119af15af2bd531566f5e4aafb1c
bel7aG <belhassen.gharsallah@isetrades.com>
2018-11-25 21:08:41 +0100

    hmmmmm easy one

---
+++ from-zero/create-table/bel7aG.sql
@@ -0,0 +1,9 @@
+DROP table bel7aG;
+
+CREATE TABLE bel7aG(
+  cin Number(8) PRIMARY KEY,  --12345678
+  name VARCHAR(9),            -- Belhassen
+  surname VARCHAR(10),        --Gharsallah
+  phone NUMBER(8),            --53882475
+  city VARCHAR(5)             --rades
+);
