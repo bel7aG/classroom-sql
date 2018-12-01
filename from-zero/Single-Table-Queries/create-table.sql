bd7c7272808c5323bcb7f902fbd89bc535f4799a
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-01 19:18:12 +0100

    create table and insert some data inside it

---
+++ from-zero/Single-Table-Queries/create-table.sql
@@ -0,0 +1,18 @@
+DROP TABLE Bel7aG;
+
+CREATE TABLE Bel7aG(
+  id Number(8) PRIMARY KEY,
+  name varchar(30),
+  surname varchar(30)
+);
+
+INSERT INTO Bel7aG
+  VALUES(17121231, 'Belhassen', 'Gharsallah');
+
+
+INSERT INTO Bel7aG
+  VALUES(14121141, 'hoho', 'habhab');
+
+
+INSERT INTO Bel7aG
+  VALUES(13121231, 'lola', 'really');
