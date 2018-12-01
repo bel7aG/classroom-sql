1a7ef7fb7d27aaed01a9321d7bb836c42c0102eb
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-01 20:50:57 +0100

    using create sequence and the distinct one

--- from-zero/Single-Table-Queries/create-table.sql
+++ from-zero/Single-Table-Queries/create-table.sql
@@ -1,18 +1,26 @@
-DROP TABLE Bel7aG;
-
-CREATE TABLE Bel7aG(
-  id Number(8) PRIMARY KEY,
-  name varchar(30),
-  surname varchar(30)
+DROP TABLE Department;
+DROP SEQUENCE seq_dno;
+CREATE TABLE Department(
+  dno NUMBER(8),
+  dName VARCHAR(70),
+  loc VARCHAR(30)
 );
 
-INSERT INTO Bel7aG
-  VALUES(17121231, 'Belhassen', 'Gharsallah');
+CREATE SEQUENCE seq_dno
+  start with 10
+  increment by 10;
+
+INSERT INTO Department
+  VALUES(seq_dno.nextval, 'Accounting', 'New York');
 
 
-INSERT INTO Bel7aG
-  VALUES(14121141, 'hoho', 'habhab');
+INSERT INTO Department
+  VALUES(seq_dno.nextval, 'Research', 'Dallas');
 
 
-INSERT INTO Bel7aG
-  VALUES(13121231, 'lola', 'really');
+INSERT INTO Department
+  VALUES(seq_dno.nextval, 'Operation', 'Berlin');
+
+
+INSERT INTO Department
+  VALUES(seq_dno.nextval, 'Sales', 'Tunisia');
