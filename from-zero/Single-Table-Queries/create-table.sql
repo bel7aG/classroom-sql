a7bacdaa5a6784eabfe644b4a78794365c6ea77d
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-01 22:09:05 +0100

    where condition in SQL :D

--- from-zero/Single-Table-Queries/create-table.sql
+++ from-zero/Single-Table-Queries/create-table.sql
@@ -21,7 +21,7 @@ INSERT INTO Department
 
 
 INSERT INTO Department
-  VALUES(seq_dno.nextval, 'Operation', 'Bermin');
+  VALUES(seq_dno.nextval, 'Operation', 'Berlin');
 
 
 INSERT INTO Department
@@ -34,19 +34,21 @@ CREATE TABLE emp(
   address VARCHAR(10),
   codeE Number(4),
   jobE VARCHAR(20),
+  comession Number(6),
   city char(15),
   tel Number(8),
+  dateE DATE,
   CONSTRAINT cli PRIMARY KEY(idE)
 );
 
-INSERT INTO emp (idE, nameE, codeE, city, tel, jobE)
-VALUES (1000, 'Mohamed', 1000, 'tunis', 1121032, 'JS DEVELOPER');
+INSERT INTO emp (idE, nameE, codeE, city, tel, jobE, comession)
+VALUES (1000, 'Belhassen', 1000, 'tunis', 1121032, 'JS DEVELOPER', 120);
 
-INSERT INTO emp (idE, nameE, codeE, city, tel, jobE)
-VALUES (1010, 'ali', 1000, 'tunis', 1235411, 'PYTHON DEVELOPER');
+INSERT INTO emp (idE, nameE, codeE, city, tel, jobE, comession)
+VALUES (1010, 'bg', 1000, 'seattle', 1235411, 'PYTHON DEVELOPER', 230);
 
-INSERT INTO emp (idE, nameE, codeE, city, tel, jobE)
-VALUES (1020, 'adel', 1010, 'ben arous', 1254225, 'ES[6..9] DEVELOPER');
+INSERT INTO emp (idE, nameE, codeE, city, tel, jobE, comession)
+VALUES (1020, 'bel7aG', 1010, 'ben arous', 1254225, 'ES[6..9] DEVELOPER', 790);
 
-INSERT INTO emp (idE, nameE, codeE, city, tel, jobE)
-VALUES (1030, 'boubaker', 1010, 'ben arous', 2541211, 'JS DEVELOPER');
+INSERT INTO emp (idE, nameE, codeE, city, tel, jobE, comession)
+VALUES (1030, 'bel7aG', 1010, 'ben arous', 2541211, 'JS DEVELOPER', 49);
--- from-zero/Single-Table-Queries/select.sql
+++ from-zero/Single-Table-Queries/select.sql
@@ -4,12 +4,12 @@
 
 SELECT jobE
   FROM emp;
-  /*JOBE
+  /* JOBE
   --------------------
   JS DEVELOPER
   PYTHON DEVELOPER
   ES[6..9] DEVELOPER
-  JS DEVELOPER*/
+  JS DEVELOPER */
 
 SELECT distinct jobE
   FROM emp;
@@ -18,3 +18,83 @@ SELECT distinct jobE
   JS DEVELOPER
   PYTHON DEVELOPER
   ES[6..9] DEVELOPER*/
+
+
+SELECT jobE, nameE
+FROM emp;
+/* JOBE                 NAMEE
+-------------------- ---------------
+JS DEVELOPER         Mohamed
+PYTHON DEVELOPER     ali
+ES[6..9] DEVELOPER   adel
+JS DEVELOPER         boubaker*/
+
+SELECT distinct jobE, nameE
+  FROM emp;
+/* JOBE                 NAMEE
+-------------------- ---------------
+JS DEVELOPER         Mohamed
+JS DEVELOPER         boubaker
+ES[6..9] DEVELOPER   adel
+PYTHON DEVELOPER     ali*/
+
+SELECT nameE, tel
+  FROM emp
+  WHERE LOWER(jobE) = LOWER('jS devEloPeR')
+  AND LOWER(nameE) = 'belhassen'
+  OR LOWER(city) = 'seattle';
+/* NAMEE                  TEL
+--------------- ----------
+Belhassen          1121032
+bg                 1235411*/
+
+
+SELECT nameE, jobE, comession
+  FROM emp
+  WHERE comession > 200;
+/* NAMEE           JOBE                  COMESSION
+--------------- -------------------- ----------
+bg              PYTHON DEVELOPER            230
+bel7aG          ES[6..9] DEVELOPER          790*/
+
+SELECT nameE, jobE, comession
+  FROM emp
+  WHERE comession < 300
+  AND UPPER(jobE) = 'JS DEVELOPER'
+  AND LOWER(nameE) != 'bel7ag';
+/* NAMEE           JOBE                  COMESSION
+--------------- -------------------- ----------
+Belhassen       JS DEVELOPER                120*/
+
+
+SELECT *
+  FROM Department
+  WHERE dno IN (20, 40);
+/*        DNO DNAME      LOC
+---------- ---------- ----------
+        20 Research   Dallas
+        40 Sales      Tunisia*/
+
+SELECT *
+  FROM Department
+  WHERE LOWER(dName) IN ('sales', 'research');
+/* DNO DNAME      LOC
+---------- ---------- ----------
+        20 Research   Dallas
+        40 Sales      Tunisia*/
+
+SELECT *
+  FROM Department
+  WHERE LOWER(UPPER) NOT IN ('SALES', 'RESEARCH');
+/*        DNO DNAME      LOC
+---------- ---------- ----------
+        10 Accounting New York
+        30 Operation  Berlin*/
+
+SELECT nameE, codeE, jobE, city, comession
+  FROM emp
+  WHERE comession BETWEEN 10 AND 150;
+/* NAMEE                CODEE JOBE                 CITY             COMESSION
+--------------- ---------- -------------------- --------------- ----------
+Belhassen             1000 JS DEVELOPER         tunis                  120
+bel7aG                1010 JS DEVELOPER         ben arous               49*/
