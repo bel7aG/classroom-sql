f47ec6b996ddbe427c83874c3967f5f3e848557b
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-02 16:53:08 +0100

    back to learn

---
+++ from-zero/Single-Row-Functions/NumericAndDateFunctions.sql
@@ -0,0 +1,32 @@
+          /*  ROUND(int)  */
+
+SELECT ROUND(22.3183134)
+  FROM DUAL;
+/* ROUND(22.3183134)
+  -----------------
+       22          */
+
+SELECT ROUND(22.500)
+ FROM DUAL;
+  /* ROUND(22.500)
+ -----------------
+      23          */
+
+
+          /* trunc(int) */
+
+SELECT TRUNC(10.9)
+  FROM DUAL;
+/* TRUNC(10.9)
+  -----------
+       10       */
+
+
+
+            /*   DATE FUNCTIONS   */
+
+SELECT sysdate
+  FROM DUAL;
+/* SYSDATE
+---------
+02-DEC-18 */
